17cbc0438d7dc99deab9942421d7b12d799d3932
Yer <kusumerkebulan@gmail.com>
2023-10-28 02:42:28 +0600

    upd6

--- components/music.tsx
+++ components/music.tsx
@@ -12,15 +12,15 @@ export default function Music() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">My Whisper Домбыра ковер.</h2>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2 className="h2 mb-4" data-aos="fade-up" data-aos-delay="200">My Whisper Домбыра ковер.</h2>
+            <p className="text-xl text-gray-400" data-aos="fade-up" data-aos-delay="300">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
 
           {/* Items */}
           <div className={"lg:flex lg:justify-start lg:items-center"}>
-            <Image className={'max-w-full mb-10  mr-16 md:max-w-none h-auto rounded-2xl'} width={540} height={405} src={MyWhisper} alt={'Music My Whisper Album'} />
+            <Image className={'max-w-full mb-10  mr-16 md:max-w-none h-auto rounded-2xl' } width={540} height={405} src={MyWhisper} data-aos="fade-right" data-aos-delay="200" alt={'Music My Whisper Album'} />
             <div>
-              <ul className={"lg:text-2xl max-sm:text-xl cursor-pointer"}>
+              <ul className={"lg:text-2xl max-sm:text-xl cursor-pointer"} data-aos="fade-left" data-aos-delay="200">
                 <li>1) Careless Whisper </li>
                 <li>2) Stranger in the night</li>
                 <li>3) Shape of my heart</li>
@@ -35,7 +35,7 @@ export default function Music() {
                 <li>12) I Just Called to Say I Love You</li>
               </ul>
               <div>
-                <ul className={"flex items-center justify-start lg:-ml-8"}>
+                <ul className={"flex items-center justify-start lg:-ml-8"} data-aos="fade-left" data-aos-delay="200">
                   <li><Image alt="Itunes" width={128} height={128} src={Apple}/></li>
                   <li><Image alt="Spotify" width={128} height={128} src={Spotify}/></li>
                   <li><Image alt="Youtube" width={128} height={128} src={Youtube}/></li>
